4d29717f9f7a64c522d2d4acb4049603d16de2b8
tmddk9445 <tmddk9445@naver.com>
2022-12-13 08:54:27 +0900

    2022-12-12 월요일
    DB(kb 수업)
    
    db_study2 테이블
    db_mySQL_study파

--- db_mySQL_study.sql
+++ db_mySQL_study.sql
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `product_mst` (
   `product_price` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `product_name` (`product_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- 테이블 데이터 db_study.product_mst:~6 rows (대략적) 내보내기
 DELETE FROM `product_mst`;
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS `student_mst` (
   `student_year` int NOT NULL DEFAULT '0',
   `score` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- 테이블 데이터 db_study.student_mst:~9 rows (대략적) 내보내기
 DELETE FROM `student_mst`;
@@ -90,7 +90,7 @@ INSERT INTO `student_mst` (`id`, `name`, `student_year`, `score`) VALUES
 	(7, '황석민', 1, 80),
 	(8, '박준현', 2, 100),
 	(9, '이승아', 3, 90),
-	(11, NULL, 1, 100);
+	(10, NULL, 1, 100);
 
 -- 테이블 db_study.study_notice_img 구조 내보내기
 DROP TABLE IF EXISTS `study_notice_img`;
@@ -155,13 +155,12 @@ CREATE TABLE IF NOT EXISTS `user_dtl` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=sjis COLLATE=sjis_bin;
 
--- 테이블 데이터 db_study.user_dtl:~4 rows (대략적) 내보내기
+-- 테이블 데이터 db_study.user_dtl:~3 rows (대략적) 내보내기
 DELETE FROM `user_dtl`;
 INSERT INTO `user_dtl` (`id`, `phone`, `address`, `create_date`, `update_date`) VALUES
+	(1, NULL, NULL, '2022-12-09 10:48:12', '2022-12-09 10:48:12'),
 	(2, NULL, NULL, '2022-12-09 10:48:12', '2022-12-09 10:48:12'),
-	(3, NULL, NULL, '2022-12-09 10:48:12', '2022-12-09 10:48:12'),
-	(4, NULL, NULL, '2022-12-09 10:48:12', '2022-12-09 10:48:12'),
-	(5, NULL, NULL, '2022-12-09 10:48:12', '2022-12-09 10:48:12');
+	(3, NULL, NULL, '2022-12-09 10:48:12', '2022-12-09 10:48:12');
 
 -- 테이블 db_study.user_mst 구조 내보내기
 DROP TABLE IF EXISTS `user_mst`;
@@ -213,9 +212,134 @@ END//
 DELIMITER ;
 SET SQL_MODE=@OLDTMP_SQL_MODE;
 
+
+-- db_study2 데이터베이스 구조 내보내기
+DROP DATABASE IF EXISTS `db_study2`;
+CREATE DATABASE IF NOT EXISTS `db_study2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_study2`;
+
+-- 테이블 db_study2.course_mst 구조 내보내기
+DROP TABLE IF EXISTS `course_mst`;
+CREATE TABLE IF NOT EXISTS `course_mst` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `subject_id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `status_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 db_study2.course_mst:~7 rows (대략적) 내보내기
+DELETE FROM `course_mst`;
+INSERT INTO `course_mst` (`id`, `subject_id`, `student_id`, `status_id`) VALUES
+	(1, 1, 1, 1),
+	(2, 2, 1, 2),
+	(3, 3, 1, 2),
+	(4, 1, 2, 2),
+	(5, 3, 2, 1),
+	(6, 1, 3, 1),
+	(7, 1, 4, 2);
+
+-- 테이블 db_study2.emp_mst 구조 내보내기
+DROP TABLE IF EXISTS `emp_mst`;
+CREATE TABLE IF NOT EXISTS `emp_mst` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `emp_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 db_study2.emp_mst:~4 rows (대략적) 내보내기
+DELETE FROM `emp_mst`;
+INSERT INTO `emp_mst` (`id`, `emp_name`) VALUES
+	(1, '문성현'),
+	(2, '윤대휘'),
+	(3, '문자영'),
+	(4, '정규민');
+
+-- 테이블 db_study2.score_mst 구조 내보내기
+DROP TABLE IF EXISTS `score_mst`;
+CREATE TABLE IF NOT EXISTS `score_mst` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `course_id` int NOT NULL DEFAULT '0',
+  `score` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 db_study2.score_mst:~5 rows (대략적) 내보내기
+DELETE FROM `score_mst`;
+INSERT INTO `score_mst` (`id`, `course_id`, `score`) VALUES
+	(1, 1, 80),
+	(2, 3, 90),
+	(3, 3, 70),
+	(4, 2, 80),
+	(5, 2, 90);
+
+-- 테이블 db_study2.status_mst 구조 내보내기
+DROP TABLE IF EXISTS `status_mst`;
+CREATE TABLE IF NOT EXISTS `status_mst` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `status_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 db_study2.status_mst:~2 rows (대략적) 내보내기
+DELETE FROM `status_mst`;
+INSERT INTO `status_mst` (`id`, `status_name`) VALUES
+	(1, '수강'),
+	(2, '수료');
+
+-- 테이블 db_study2.student_dtl 구조 내보내기
+DROP TABLE IF EXISTS `student_dtl`;
+CREATE TABLE IF NOT EXISTS `student_dtl` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `emp_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 db_study2.student_dtl:~5 rows (대략적) 내보내기
+DELETE FROM `student_dtl`;
+INSERT INTO `student_dtl` (`id`, `emp_id`) VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 4),
+	(5, 3);
+
+-- 테이블 db_study2.student_mst 구조 내보내기
+DROP TABLE IF EXISTS `student_mst`;
+CREATE TABLE IF NOT EXISTS `student_mst` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `student_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 db_study2.student_mst:~5 rows (대략적) 내보내기
+DELETE FROM `student_mst`;
+INSERT INTO `student_mst` (`id`, `student_name`) VALUES
+	(1, '신경수'),
+	(2, '고희주'),
+	(3, '장건녕'),
+	(4, '문승주'),
+	(5, '이승아');
+
+-- 테이블 db_study2.subject_mst 구조 내보내기
+DROP TABLE IF EXISTS `subject_mst`;
+CREATE TABLE IF NOT EXISTS `subject_mst` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `course_name` (`course_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 db_study2.subject_mst:~4 rows (대략적) 내보내기
+DELETE FROM `subject_mst`;
+INSERT INTO `subject_mst` (`id`, `course_name`) VALUES
+	(3, 'C'),
+	(1, 'java'),
+	(2, 'python'),
+	(4, 'web');
+
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
-information_schemaorder_dtl
